0cec262cfc5b0b1cd5d897033c374cf527c5b204
Ryan McCullagh <rmccullagh@users.noreply.github.com>
2013-12-26 19:53:05 -0600

    Update __init__.php

--- __init__.php
+++ __init__.php
@@ -18,7 +18,7 @@
 define('BASE_PATH', realpath(dirname(__FILE__)));
 require __DIR__.'/bootstrap/autoload.php';
 
-$request 		= new CLI\Request($argv);
+$request    = new CLI\Request($argv);
 $dispatcher = new CLI\Dispatcher($request);
 
 $dispatcher->prepare();
